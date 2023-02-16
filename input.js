25c0486948a6f49f0f006e5a2d6c5cd80c18fd39
Spencer Hahmo <schahmo@gmail.com>
2023-02-15 17:15:24 -0700

    Enables the user to move the snake, and allows the user exit the program with control c

--- input.js
+++ input.js
@@ -1,18 +1,48 @@
+// Stores the active TCP connection object.
+let connection;
+
 const handleUserInput = function (key) {
   // If the user enters control c, play.js stops
   if (key === '\u0003') {
     process.exit();
   }
+
+  // If the user presses w
+  if (key === 'w') {
+    connection.write('Move: up');
+    //console.log("Move: up")
+  }
+  
+  // If the user presses a
+  if (key === 'a') {
+    //console.log("Move: left")
+    connection.write('Move: left');
+  }
+  
+  // If the user presses s
+  if (key === 's') {
+    //console.log("Move: down")
+    connection.write('Move: down');
+  }
+  
+  // If the user presses d
+  if (key === 'd') {
+    //console.log("Move: right")
+    connection.write('Move: right');
+  }
 }
+
 // setup interface to handle user input from stdin
 
-const setupInput = function () {
+const setupInput = (conn) => {
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
-  
+
   stdin.on("data", handleUserInput);
-  
+
   stdin.resume();
   return stdin;
 };
